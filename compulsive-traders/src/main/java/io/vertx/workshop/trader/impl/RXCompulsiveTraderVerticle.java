9b951127ae0bd0b618d0c8fc540d48bd90af41a8
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-05-06 17:53:25 -0400

    9 completed

--- compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/RXCompulsiveTraderVerticle.java
+++ compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/RXCompulsiveTraderVerticle.java
@@ -3,6 +3,7 @@ package io.vertx.workshop.trader.impl;
 import io.reactivex.Single;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.CompletableHelper;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.eventbus.MessageConsumer;
 import io.vertx.reactivex.servicediscovery.ServiceDiscovery;
@@ -33,22 +34,26 @@ public class RXCompulsiveTraderVerticle extends AbstractVerticle {
             //----
 
             // TODO 1 - "wait" for both single to be completed (using Single.zip or Single.zipWith methods)
+            retrieveThePortfolioService.zipWith(retrieveTheMarket, (ps, consumer) -> {
 
-            // TODO 2 - When both single have completed, attach the handler to the message consumer to execute the
-            // trading logic
+                        // TODO 2 - When both single have completed, attach the handler to the message consumer to execute the
+                        // trading logic
 
-            // TODO 3 - Use the TraderUtils.drumpTradingLogic method returning a Completable. Don't forget to
-            // subscribe to it, or nothing will happen. Return "true" to comply with the "zip" operator
-            // signature.
+                        // TODO 3 - Use the TraderUtils.drumpTradingLogic method returning a Completable. Don't forget to
+                        // subscribe to it, or nothing will happen. Return "true" to comply with the "zip" operator
+                        // signature.
 
-
+                        consumer.handler(message ->
+                                TraderUtils.dumbTradingLogic(company, numberOfShares, ps, message.body()).subscribe());
+                        return true;
+                    })
             // TODO 4 - Transform the output into a Completable (toCompletable) and subscribe to it using:
             //.subscribe(CompletableHelper.toObserver(future)) - it reports the failure or success to the `done`
             // future.
             
             // To remove
-            future.complete();
-            //----
+                    .toCompletable()
+                    .subscribe(CompletableHelper.toObserver(future));            //----
 
         });
     }
